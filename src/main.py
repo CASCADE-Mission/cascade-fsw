fa8fe3abcd2294a428300f18bfa1cfb0ef74096a
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-03-25 22:27:19 -0400

    Remove example function

--- src/main.py
+++ src/main.py
@@ -4,16 +4,9 @@
 # Import main process
 from process import Process
 
-def something():
-    for i in range(20):
-        print(f"something: {i}")
-
 if __name__ == "__main__":
     # Initialize the main process
     cascade = Process()
-
-    cascade.add(something)
-    cascade.add(something)
 
     # Execute the main process
     cascade.run()
